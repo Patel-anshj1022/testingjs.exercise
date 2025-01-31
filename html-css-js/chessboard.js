2e469ea0dade09fc817a8e680b387b84aa5a8c3b
Patel-anshj1022 <patelanshj950@gmail.com>
2025-01-31 15:44:27 +0300

    add chessboard.js

---
+++ html-css-js/chessboard.js
@@ -0,0 +1,16 @@
+function createChessboard() {
+    let chessboard = "";
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if ((i + j) % 2 === 0) {
+          chessboard += "#";
+        } else {
+          chessboard += " ";
+        }
+      }
+      chessboard += "\n";
+    }
+    return chessboard;
+  }
+  
+  console.log(createChessboard());
